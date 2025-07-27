ea2f8e84e23016629a6ab4611156ecad90715bb6
Anonymous Author <anonymous@example.com>
2025-07-27 06:24:22 +0000

    fix: use correct base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: "/2025-consistency",
+	base: "/SRC",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
